6a1a588cd5ee8d55e081aff0028529345a03dd43
jswesner <jeff.wesner@usd.edu>
2023-02-07 13:11:04 -0600

    updated code to make fish and invert data

---
+++ data/raw_data/fish_and_inverts/01_combine_macros_fish.R
@@ -0,0 +1,163 @@
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(hydroTSM)
+
+# load data
+macro_dw = readRDS(file = "data/derived_data/inverts_dw-allyears.rds")
+fish_dw = readRDS(file = "data/derived_data/fish_dw-allyears.rds")
+
+gpp = readRDS("data/derived_data/gpp_means.rds") %>% clean_names() %>% 
+  rename(gpp = mean, gpp_sd = sd) %>% 
+  mutate(log_gpp = log(gpp),
+         log_gpp_s = scale(log_gpp, center = T, scale = T),
+         log_gpp_s = as.numeric(log_gpp_s),
+         gpp_s = (gpp - mean(gpp))/sd(gpp)) 
+
+mat = readRDS("data/derived_data/temperature_mean-annual.rds") %>% 
+  mutate(mat_s = (mean - mean(mean))/sd(mean)) %>% clean_names()
+
+
+fish_collections = fish_dw %>% ungroup %>% distinct(mean_julian, event_id, site_id) %>%
+  rename(fish_site_id = site_id,
+         fish_event_id = event_id,
+         fish_julian = mean_julian)
+
+macro_collections = macro_dw %>% ungroup %>% distinct(julian, event_id, site_id) %>% 
+  rename(macro_event_id = event_id,
+         macro_site_id = site_id,
+         macro_julian = julian)
+
+
+# the tally's and filters just limit the fish/macro pairings to one sample 
+# per bout (i.e., if fish were sampled in June 15 and macros on June 2 and 
+# June 20, then we only keep the June 20 macro sample and combine it with fish on June 15)
+# Otherwise we'd have replicated the same fish sample twice
+
+events_to_keep = fish_collections %>% 
+  expand_grid(macro_collections) %>% 
+  mutate(date_diff = abs(fish_julian - macro_julian)) %>% 
+  filter(fish_site_id == macro_site_id) %>% 
+  filter(date_diff <= 30) %>%  # only samples that are within 30 days of each other
+  group_by(fish_event_id) %>% 
+  add_tally() %>%         
+  filter(date_diff == min(date_diff)) %>%
+  ungroup %>% 
+  select(-n) %>% 
+  group_by(macro_event_id) %>% 
+  add_tally() %>% 
+  filter(date_diff == min(date_diff)) %>% 
+  ungroup %>% 
+  mutate(sample_id = 1:nrow(.))
+
+# filter to matched "events"
+fish_dw_filtered = fish_dw %>% 
+  semi_join(events_to_keep %>% 
+              rename(event_id = fish_event_id) %>% 
+              select(event_id)) 
+
+macro_dw_filtered = macro_dw %>% 
+  semi_join(events_to_keep %>% 
+              rename(event_id = macro_event_id) %>% 
+              select(event_id)) %>% 
+  mutate(year = year(date),
+         month = month(date))
+
+# ensure that there are no duplicate samples
+fish_dw_filtered %>% 
+  ungroup %>% 
+  distinct(month, year, site_id) %>%
+  group_by(month, year, site_id) %>% 
+  add_count() %>% 
+  filter(n>1)
+
+macro_dw_filtered %>% 
+  mutate(month = month(date),
+         year = year(date)) %>% 
+  ungroup %>% 
+  distinct(month, year, site_id) %>%
+  group_by(month, year, site_id) %>% 
+  add_count() %>% 
+  filter(n>1)
+
+fish_dw_wrangled = fish_dw_filtered %>% 
+  rename(fish_event_id = event_id) %>% 
+  left_join(events_to_keep %>% distinct(fish_event_id, sample_id, macro_julian)) %>% # macro date not fish date so all are the same
+  group_by(dw, site_id, sample_id, macro_julian) %>% 
+  summarize(no_m2 = sum(no_m2)) %>% 
+  ungroup() %>% 
+  mutate(xmin = min(dw)) %>% 
+  group_by(site_id) %>% 
+  mutate(xmax = max(dw)) %>% 
+  ungroup %>% 
+  left_join(gpp) %>% 
+  left_join(mat) %>% 
+  filter(!is.na(log_gpp_s)) %>% 
+  mutate(date = as_date(macro_julian),
+         year = year(date),
+         yday = yday(date),
+         season = time2season(date, out.fmt = "seasons")) %>% 
+  mutate(sample_int = as.integer(as.factor(sample_id)),
+         year_int = as.integer(as.factor(year)),
+         site_int = as.integer(as.factor(site_id)),
+         season_int = as.integer(as.factor(season))) 
+
+macro_dw_wrangled = macro_dw_filtered %>% 
+  rename(macro_event_id = event_id) %>% 
+  left_join(events_to_keep %>% distinct(macro_event_id, sample_id, macro_julian)) %>% 
+  group_by(dw, site_id, sample_id, macro_julian) %>% 
+  summarize(no_m2 = sum(no_m2)) %>% 
+  ungroup() %>% 
+  mutate(xmin = min(dw)) %>% 
+  group_by(site_id) %>% 
+  mutate(xmax = max(dw)) %>% 
+  ungroup %>% 
+  left_join(gpp) %>% 
+  left_join(mat) %>% 
+  filter(!is.na(log_gpp_s)) %>% 
+  mutate(date = as_date(macro_julian),
+         year = year(date),
+         yday = yday(date),
+         season = time2season(date, out.fmt = "seasons")) %>% 
+  mutate(sample_int = as.integer(as.factor(sample_id)),
+         year_int = as.integer(as.factor(year)),
+         site_int = as.integer(as.factor(site_id)),
+         season_int = as.integer(as.factor(season))) 
+
+macro_fish_dw = bind_rows(fish_dw_wrangled, macro_dw_wrangled) %>% 
+  group_by(dw, site_id, sample_id, macro_julian) %>% 
+  summarize(no_m2 = sum(no_m2)) %>% 
+  ungroup() %>% 
+  mutate(xmin = min(dw)) %>% 
+  group_by(site_id) %>% 
+  mutate(xmax = max(dw)) %>% 
+  ungroup %>% 
+  left_join(gpp) %>% 
+  left_join(mat) %>% 
+  filter(!is.na(log_gpp_s)) %>% 
+  mutate(date = as_date(macro_julian),
+         year = year(date),
+         yday = yday(date),
+         season = time2season(date, out.fmt = "seasons")) %>% 
+  mutate(sample_int = as.integer(as.factor(sample_id)),
+         year_int = as.integer(as.factor(year)),
+         site_int = as.integer(as.factor(site_id)),
+         season_int = as.integer(as.factor(season))) 
+
+
+saveRDS(fish_dw_wrangled, file = "data/derived_data/fish_dw-wrangled.rds")
+saveRDS(macro_dw_wrangled, file = "data/derived_data/macro_dw-wrangled.rds")
+saveRDS(macro_fish_dw, file = "data/derived_data/fish_inverts_dw-allyears.rds")
+
+
+# sanity check ------------------------------------------------------------
+
+macro_fish_dw %>% 
+  group_by(dw) %>% 
+  summarize(no_m2 = sum(no_m2)) %>% 
+  ggplot(aes(x = dw, y = no_m2)) +
+  # geom_histogram(bins = 200) +
+  geom_point() +
+  scale_x_log10() +
+  scale_y_log10() +
+  NULL
---
+++ data/raw_data/inverts/00_make_macros_data.R
@@ -0,0 +1,122 @@
+# Download Macroinvertebrate data and estimate dry weights
+library(neonUtilities)
+library(tidyverse)
+library(janitor)
+library(lubridate)
+library(tidybayes)
+library(brms)
+library(neonstore)
+library(neonDivData)
+source("code/sandbox/estimate_macro_dw_functions.R")
+
+# load Length Weight coefficient table (used below)
+coeff <- read.csv("data/macro_lw_coeffs.csv")
+stream_sites <- readRDS("data/streams.rds")
+
+# directory
+Sys.setenv(NEONSTORE_HOME = paste(getwd(), 
+                                  "/data",
+                                  sep=""))
+
+# download data (takes ~15 minutes) --------------------------------
+#stream sites
+streamsites=c("HOPB", "LEWI", "POSE", "CUPE",
+              "GUIL", "KING", "MCDI", "LECO",
+              "WALK", "MAYF", "ARIK", "BLUE",
+              "PRIN", "BLDE", "COMO", "WLOU", 
+              "SYCA", "REDB", "MART", "MCRA",
+              "BIGC", "TECR", "OKSR", "CARI")
+
+# neon_download(product="DP1.20120.001",
+#               start_date=NA,
+#               end_date=NA,
+#               type="basic",
+#               site= NA)
+
+# stack data
+# inverts_stacked = stackFromStore(filepaths=neon_dir(),
+#                                   dpID="DP1.20120.001",
+#                                   package="basic",
+#                                   site = streamsites)
+
+# saveRDS(inverts_stacked, file = "data/inverts_stacked.rds")
+
+inverts_stacked = readRDS(file = "data/inverts_stacked.rds")
+macro = readRDS(file = "data/inverts_stacked.rds")
+
+# 2) Add LW coefficients, estimate dry weights  ------------------------------------
+
+# add length weight coefficients by taxon
+MN.lw <- LW_coef(x = macro$inv_taxonomyProcessed,
+                 lw_coef = coeff,
+                 percent = TRUE)
+
+# questionable measurements ####
+# filter out individuals that were "damaged" and measurement was affected
+# this is a flag which is added by NEON
+# MN.no.damage <- MN.lw %>%
+#   filter(!str_detect(sampleCondition,
+#                      "measurement")) %>%
+#   est_dw(fieldData = macro$inv_fieldData)
+
+
+# length_weight estimates -------------------------------------------------
+
+
+
+# est_dw <- function(x, fieldData){
+# x = inv_taxonomyProcessed table from NEON data product "DP1.20120.001" with LW coefficients added using the LW_coeff() function
+# fieldData = inv_fieldData table from NEON data product "DP1.20120.001"
+
+MN.lw.x = MN.lw %>%
+  filter(!str_detect(sampleCondition,
+                     "measurement"))
+fieldData = macro$inv_fieldData
+
+# simplify fieldData to three columns
+field = fieldData %>%
+  select(siteID, sampleID, benthicArea) %>%
+  distinct()
+# add benthicArea column from fieldData to x. This is necessary to calculate number per m2 below
+
+# join by siteID and sampleID
+MN.no.damage <- left_join(MN.lw.x, field, by = c("siteID", "sampleID")) %>% 
+  # filter(invertebrateLifeStage %in% c("larva", "pupa")) %>%
+  filter(sizeClass >= 3) %>%
+  mutate(dw = case_when(formula_type == 1 ~ a * sizeClass^b,
+                        formula_type == 2 ~ exp(a + b * log(sizeClass)))) %>% 
+  group_by(dw, siteID, collectDate) %>% 
+  summarize(n = sum(estimatedTotalCount),
+            benthicArea = sum(benthicArea),
+            no_m2 = n/benthicArea)
+
+# clean and save ---------------------------------
+# filter out NA values in dw
+MN.no.damage.nona <- MN.no.damage %>%
+  filter(!is.na(dw), !is.na(no_m2)) %>% 
+  as_tibble()
+
+############ Data before 2020 contains duplicates according to NEON :
+#https://www.neonscience.org/impact/observatory-blog/duplicate-macroinvertebrate-collection-dp120120001-records:
+# This is supposed to be fixed in the 2022 release. The code below deletes duplicates anyway
+
+# make unique group index for siteID:collectDate combo
+MN.no.damage.unique <- MN.no.damage.nona %>%  
+  mutate(year = year(collectDate),
+         month = month(collectDate)) %>%
+  mutate(event_id = 
+           paste(siteID, year, month, sep = "_"),
+         date = ymd(as.Date(collectDate)),
+         animal_type = "macroinvertebrates") %>%
+  arrange(event_id) %>%
+  select(-year, -month) %>% 
+  clean_names()
+
+
+macro_dw = MN.no.damage.unique  %>%
+  group_by(dw, event_id, site_id, date, animal_type) %>% 
+  summarize(n = sum(n),
+            no_m2 = sum(no_m2)) %>% 
+  mutate(julian = julian(date))
+
+saveRDS(macro_dw, file = "data/macro_dw_allyears.rds")
