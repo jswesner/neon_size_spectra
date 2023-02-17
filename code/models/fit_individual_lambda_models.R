79715f3578705ffcf306d8e344de2c1a174862f2
jswesner <jeff.wesner@usd.edu>
2023-02-17 15:07:33 -0600

    add fish and invert-specific models

--- code/models/fit_individual_lambda_models.R
+++ code/models/fit_individual_lambda_models.R
@@ -8,6 +8,12 @@ rstan_options(threads_per_chain = 1)
 neon_sizes_2016_2021 = readRDS(file = "data/derived_data/fish_inverts_dw-allyears.rds") %>% 
   filter(year >= 2016 & year <= 2021)
 
+# neon_sizes_2016_2021 = readRDS(file = "data/derived_data/fish_dw-wrangled.rds") %>% 
+#   filter(year >= 2016 & year <= 2021)
+# 
+# neon_sizes_2016_2021 = readRDS(file = "data/derived_data/macro_dw-wrangled.rds") %>% 
+#   filter(year >= 2016 & year <= 2021)
+
 # compile model
 stan_spectra_single_models = stan_model("models/stan_spectra_singlesample.stan")
 
@@ -15,7 +21,7 @@ stan_spectra_single_models = stan_model("models/stan_spectra_singlesample.stan")
 # make data and fit model ---------------------------------------------------------
 
 dat_group = neon_sizes_2016_2021 %>% 
-  filter(sample_id == 10)
+  filter(sample_id == 5)
 
 stan_data = list(N = nrow(dat_group),
                              mat_s = dat_group$mat_s,
