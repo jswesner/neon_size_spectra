089d79d03ba8300a11073346a4816821afc118bc
Jpomz <jfpomeranz@gmail.com>
2023-11-02 10:12:00 -0600

    aquasync data with new sampling_method column

--- data/derived_data/aqua-sync_data/macro-size-taxonomy-data.R
+++ data/derived_data/aqua-sync_data/macro-size-taxonomy-data.R
@@ -31,6 +31,68 @@ dat %>%
   unique() %>%
   write_csv("data/derived_data/aqua-sync_data/invertebrate-taxonomy.csv")
 
+
+# From Wesner ####
+macro <- readRDS("data/raw_data/macro.rds") # to get collection info
+
+# get sampler type for each sampleID
+
+sampler = macro$inv_fieldData %>%
+  distinct(sampleID, samplerType, habitatType, substratumSizeClass) %>%
+  rename(method = samplerType)
+
+
+# join sample type to dat
+
+sampler_dat <- dat %>%
+  left_join(sampler) %>%
+  filter(!is.na(method)) %>%
+  mutate(date = ymd(as.Date(collectDate)))
+
+# copied from 00_make_macros_data.R script
+size_dat <- sampler_dat %>% 
+  group_by(dw, siteID, sampleID, collectDate,
+           scientificName, sizeClass, benthicArea,
+           method, habitatType, substratumSizeClass) %>% 
+  summarize(
+    # total number of individuals from sample
+    # this is based on the % subsample in the original processing
+    n = sum(estimatedTotalCount, na.rm = TRUE))%>%
+  filter(!is.na(dw)) %>%
+  mutate(organism_group = "macroinvertebrate") 
+
+# select columns necessary for formatting
+size_dat <- size_dat %>%
+  ungroup() %>%
+  select(siteID, sampleID, method,
+         collectDate, benthicArea,
+         organism_group, scientificName, dw, sizeClass, n)
+names(size_dat)
+
+
+# rename existing columns and add new ones to match aquaSync format
+short_dat <- size_dat %>%
+  mutate(date = as_date(collectDate),
+         body_weight_units = "mg",
+         body_length_units = "mm") %>%
+  tidyr::unite("site", c(siteID, date), remove = FALSE) %>%
+  rename(sampling_area = benthicArea,
+         taxon = scientificName,
+         body_mass = dw,
+         body_length = sizeClass) %>%
+  select(-collectDate)
+
+# extract info on site-date combo
+# use this to construct site metadate
+site_date <- short_dat %>%
+  select(site, siteID, date, method) %>%
+  unique() %>%
+  mutate(sampling_year = year(date),
+         sampling_month = month(date))
+# save as csv
+write_csv(site_date, "data/derived_data/aqua-sync_data/site_date.csv")
+
+
 # template column names:
 # site = site name, code
 # sampling_method = Surber, kick, etc
@@ -45,65 +107,21 @@ dat %>%
 # count = number of individuals
 # multiplier = 1/sample area, multiplier needed for count per m2 for that sample
 
-# copied from 00_make_macros_data.R script
-size_dat <- dat %>% 
-  group_by(dw, siteID, sampleID, collectDate,
-           scientificName, sizeClass, benthicArea) %>% 
-  summarize(
-    # total number of individuals from sample
-    # this is based on the % subsample in the original processing
-    n = sum(estimatedTotalCount, na.rm = TRUE))%>%
-  filter(!is.na(dw)) %>%
-  mutate(organism_group = "macroinvertebrate") 
-
-# select columns necessary for formatting
-size_dat <- size_dat %>%
-  ungroup() %>%
-  select(siteID, sampleID, collectDate, benthicArea,
-         organism_group, scientificName, dw, sizeClass, n)
-
-# separate sampleID column to get sample number and 
-# sample method
-# sampleID column does not have uniform number of characters
-# below separate() commands are a bit convoluted, but they work for this data set
-# double check if doing this again on future data
-sample_method <- size_dat %>%
-  separate(sampleID, into = c("sampleID", "sample"), -1 ) %>%
-  separate(sampleID, into = c("beginning", "end"), 9) %>%
-  separate(end, into = c("num", "sampling_method")) %>%
-  select(-beginning, -num)
-
-names(sample_method)
-
-# rename existing columns and add new ones to match aquaSync format
-short_dat <- sample_method %>%
-  mutate(date = as_date(collectDate),
-         body_weight_units = "mg",
-         body_length_units = "mm") %>%
-  tidyr::unite("site", c(siteID, date), remove = FALSE) %>%
-  rename(sampling_area = benthicArea,
-         taxon = scientificName,
-         body_mass = dw,
-         body_length = sizeClass) %>%
-  select(-collectDate)
-
-# extract info on site-date combo
-# use this to construct site metadate
-site_date <- short_dat %>%
-  select(site, siteID, date) %>%
-  unique() %>%
-  mutate(sampling_year = year(date),
-         sampling_month = month(date))
-# save as csv
-write_csv(site_date, "data/derived_data/aqua-sync_data/site_date.csv")
-
-
-# extend data set based on count of body suze classes in "n" column
+# extend data set based on count of body size classes in "n" column
 # this makes each row of data be for 1 individual
+# chnage names and organize to be in AquaSync format
 size_dat_formatted <- short_dat %>%
   uncount(n, ) %>%
   mutate(count = 1,
-         multiplier = 1 / sampling_area)
+         multiplier = 1 / sampling_area) %>%
+  select(-siteID) %>%
+  rename(sample = sampleID,
+         sampling_method = method) %>%
+  select(site, sampling_method, sample, sampling_area,
+         organism_group, taxon, body_mass, body_length, body_weight_units,
+         body_length_units, count,  multiplier)
+
+names(size_dat_formatted)
 
 View(size_dat_formatted)
 
